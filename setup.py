928c0aae4e58745c756fa78bc2e77b2f59891393
awr1u24 <awr1u24@soton.ac.uk>
2025-07-01 20:02:05 +0100

    Fix: Add package data configuration for dictionary files

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
+    name="seram_tokenizer",
+    version="0.1.0",
+    author="Abdul Wahid Rukua",
+    author_email="rukuaabdulwahid@gmail.com",
+    description="Tokenizer for Seram (Geser, Gorom) language",
     packages=find_packages(),
+    package_data={
+        'seram_tokenizer': ['data/*'],
+    },
     include_package_data=True,
+    install_requires=[
+        "regex",
+    ],
+    python_requires=">=3.7",
 )
