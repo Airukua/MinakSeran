a10234dffc72a216098171b30cba0483204b14bd
awr1u24 <awr1u24@soton.ac.uk>
2025-07-01 19:26:05 +0100

    add pyproject.toml and update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='seram_tokenizer',
-    version='0.1.0',
-    author='Abdul Wahid Rukua',
-    author_email='rukuaabdulwahid@gmail.com',
-    url='https://github.com/Airukua/MinakSeran.git',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'regex',
-    ],
-    description='Tokenizer for Seram (Geser, Gorom) language',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.7',
 )
