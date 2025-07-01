0b6cd0110e7f7b828ae96f006fba01cb9d283a78
awr1u24 <awr1u24@soton.ac.uk>
2025-07-01 19:15:34 +0100

    fix name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='seram tokenizer',
+    name='seram_tokenizer',
     version='0.1.0',
     author='Abdul Wahid Rukua',
     author_email='rukuaabdulwahid@gmail.com',
